d064d5a39e7cef7b74ddf539fed60ea451d526c3
soumyaj <soumyaj@ccs.neu.edu>
2016-09-19 17:18:09 -0400

    Changes to app.js

--- test/app.js
+++ test/app.js
@@ -4,14 +4,13 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
-
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
+    var connectionString = 'mongodb://localhost:27017/test';
+    if(process.env.MLAB_DB_USERNAME) {
+        connectionString = process.env.MLAB_DB_URL_INIT +
+            process.env.MLAB_DB_USERNAME + ":" +
+            process.env.MLAB_DB_PASSWORD +
+            process.env.MLAB_DB_URL_END + '/' +
+            process.env.MLAB_DB_NAME;
     }
 
     var mongoose = require("mongoose");
