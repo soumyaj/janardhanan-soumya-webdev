57cce26adf7ea14b713dc988b4f8d82b8e3efd90
soumyaj <soumyaj@ccs.neu.edu>
2016-12-06 01:04:10 -0500

    Commented out project related code, for now

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 require ("./assignment/app.js")(app);
-require ("./project/app.js")(app);
+//require ("./project/app.js")(app);
 
 
 var ipaddress = process.env.IP;
