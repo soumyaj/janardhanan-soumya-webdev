3726d8ba85f0a47f1743dcfdb50a685e01de44f2
soumyaj <soumyaj@ccs.neu.edu>
2016-12-06 01:09:19 -0500

    Adding model.server.js

--- assignment/model/model.server.js
+++ assignment/model/model.server.js
@@ -1,8 +1,16 @@
 module.exports = function() {
 
     var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/webappmaker');
-
+    var connectionString = 'mongodb://localhost:27017/webappmaker';
+    if(process.env.MLAB_DB_USERNAME) {
+        connectionString = process.env.MLAB_DB_URL_INIT +
+            process.env.MLAB_DB_USERNAME + ":" +
+            process.env.MLAB_DB_PASSWORD +
+            process.env.MLAB_DB_URL_END + '/' +
+            process.env.MLAB_DB_NAME;
+    }
+    //mongoose.connect('mongodb://localhost/webappmaker');
+    mongoose.connect(connectionString)
     // var mwebsiteModel = require("./website/website.model.server.js")();
     // var mwidgetModel = require("./widget/widget.model.server.js")();
     // var mpageModel = require("./page/page.model.server.js")();
